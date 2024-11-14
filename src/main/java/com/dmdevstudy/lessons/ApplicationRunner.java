b416c858b0861fb3769a334bcc2e60ab42731b90
Oleg Vasilev <vasek_tr@mail.ru>
2024-11-14 20:33:39 +0300

    lesson 4 finished - dependency injection in constructor XML

--- src/main/java/com/dmdevstudy/lessons/ApplicationRunner.java
+++ src/main/java/com/dmdevstudy/lessons/ApplicationRunner.java
@@ -1,14 +1,15 @@
 package com.dmdevstudy.lessons;
 
 import com.dmdevstudy.lessons.database.pool.ConnectionPool;
+import com.dmdevstudy.lessons.service.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
-
-        //TODO 12/11/2024
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
--- src/main/java/com/dmdevstudy/lessons/service/CompanyRepository.java
+++ src/main/java/com/dmdevstudy/lessons/service/CompanyRepository.java
@@ -4,7 +4,11 @@ import com.dmdevstudy.lessons.database.pool.ConnectionPool;
 
 public class CompanyRepository {
     private final ConnectionPool connectionPool;
-    public CompanyRepository(ConnectionPool connectionPool) {
+    private CompanyRepository(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
+    }
+    public static CompanyRepository of(ConnectionPool connPool)
+    {
+        return new CompanyRepository(connPool);
     }
 }
